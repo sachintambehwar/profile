425d26f86d6c2f222288d8868100ec2ec0982741
Sachintambeshwar <sachintambeshwar496@gmail.com>
2024-06-24 22:49:54 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Layout from "./components/Layout.jsx";
+import Contact from "./components/Contact.jsx";
+import About from "./components/About.jsx";
+import Home from "./components/Home.jsx";
+import Skill from "./components/Skill.jsx";
+import Work from "./components/Work.jsx";
+import { Toaster } from "react-hot-toast";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/skill",
+        element: <Skill />,
+      },
+      {
+        path: "/work",
+        element: <Work />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <Toaster
+      toastOptions={{
+        success: {
+          style: {
+            background: "#20b00af8",
+            color: "#fff",
+          },
+        },
+        error: {
+          style: {
+            background: "#e54110",
+            color: "#fff",
+          },
+        },
+      }}
+    />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
